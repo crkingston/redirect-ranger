4db57541ea1f72bf383b3d2a75e105ea54393ead
Charlie Kingston <crkingston@outlook.com>
2023-04-26 13:53:49 +0100

    Basic app with Slack redirects

---
+++ background.js
@@ -0,0 +1,16 @@
+const urlPatterns = [
+    '.slack.com/archives/'
+    // Add more URL patterns here
+];
+  
+function shouldCloseTab(url) {
+    return urlPatterns.some(pattern => url.includes(pattern));
+}
+  
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && shouldCloseTab(changeInfo.url)) {
+        setTimeout(() => {
+            chrome.tabs.remove(tabId);
+        }, 1000);
+}   
+});
